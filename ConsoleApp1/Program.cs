8d4ca8cd603a2b360c2cec6bda780fc02b3cdfd2
Wouter Huysentruit <wouter.huysentruit@cognistreamer.com>
2018-02-06 20:03:07 +0100

    Net462 and NetStandard mix

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = HttpClientFactory.CreateClient();
+            client.Dispose();
+        }
+    }
+}
